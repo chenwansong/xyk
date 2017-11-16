f366da7ad0725e347ad130f6fd754570149fdab1
陈万松 <>
2017-11-16 16:58:39 +0800

    1.0.0

--- pages/lilv/lilv.js
+++ pages/lilv/lilv.js
@@ -7,15 +7,33 @@ Page({
 
     /**
      * 页面的初始数据
+     *
+     *   focusMonthAmount: false,
+     *   focusMonthInterest: false,
+     *   totalAmount: '',
+     *   month: '',
+     *   monthAmount: '',
+     *   monthInterest: '',
+     *   type: '',
+     *   trueInterest: '',
+     *   surfaceInterest: ''
+     *
      */
     data: {
-        focusMonthAmount: false,
-        focusMonthInterest: false,
-        totalAmount: '',
-        month: '',
-        monthAmount: '',
-        monthInterest: '',
-        type: ''
+        focusMonthAmount: false,//
+        focusMonthInterest: false,//
+        totalAmount: '',//总本金
+        month: '',//分期月数
+        monthAmount: '',//每月还款金额
+        monthInterest: '',//月利率
+        type: '',//计算方式：1月还款额，2单期利率
+
+        mingyinianlilv: '',//名义年利率
+        yuebenjin: '',//每月本金
+        yuelixi: '',//每月利息
+        zonglixi: '',//总利息
+        zonghuankuanjine: '',//总还款金额
+        trueInterest: ''//真实年利率
 
     },
 
@@ -117,6 +135,24 @@ Page({
 
     },
 
+    resetForm: function () {
+        this.setData({
+            focusMonthAmount: false,//
+            focusMonthInterest: false,//
+            totalAmount: '',//总本金
+            month: '',//分期月数
+            monthAmount: '',//每月还款金额
+            monthInterest: '',//月利率
+            type: '',//计算方式：1月还款额，2单期利率
+            mingyinianlilv: '',//名义年利率
+            yuebenjin: '',//每月本金
+            yuelixi: '',//每月利息
+            zonglixi: '',//总利息
+            zonghuankuanjine: '',//总还款金额
+            trueInterest: ''//真实年利率
+        })
+    },
+
     showToptips(error) {
         const hideToptips = $wuxToptips.show({
             timer: 3000,
@@ -131,13 +167,6 @@ Page({
     submitForm: function (e) {
         const params = e.detail.value
 
-        console.log("submitForm--totalAmount=" + this.data.totalAmount)
-        console.log("submitForm--month=" + this.data.month)
-        console.log("submitForm--monthAmount=" + this.data.monthAmount)
-        console.log("submitForm--monthInterest=" + this.data.monthInterest)
-        console.log("submitForm--type=" + this.data.type)
-
-
         if (!this.WxValidate.checkForm(e)) {
             const error = this.WxValidate.errorList[0]
             this.showToptips(error)
@@ -149,6 +178,48 @@ Page({
         //     text: '提交成功',
         // })
 
+
+        if (this.data.type == 1) {
+            //知道月还款额
+            var yuebenjin = (this.data.totalAmount / this.data.month).toFixed(2) //每月本金
+            var yuelixi = this.data.monthAmount - yuebenjin //每月利息
+            var yuelilv = yuelixi / this.data.totalAmount * 100 //月利率
+            var zonghuankuanjine = (this.data.monthAmount * this.data.month).toFixed(2)//总还款金额
+            var zonglixi = this.data.month * yuelixi//总利息
+
+            var mingyinianlilv = yuelilv * 12  //名义年利率
+            var trueInterest = (24 * this.data.month * yuelilv / (parseInt(this.data.month) + 1)).toFixed(2)
+            this.setData(
+                {
+                    mingyinianlilv: mingyinianlilv + "%",//名义年利率
+                    trueInterest: trueInterest + "%",//实际年利率
+                    yuebenjin: yuebenjin,//每月本金
+                    yuelixi: yuelixi,//每月利息
+                    zonglixi: zonglixi,//总利息
+                    zonghuankuanjine: zonghuankuanjine,//总还款金额
+                }
+            )
+
+        } else if (this.data.type == 2) {
+            //知道单期利率
+            var mingyinianlilv = (this.data.monthInterest * 12 ).toFixed(2) //名义年利率
+            var yuebenjin = (this.data.totalAmount / this.data.month).toFixed(2) //每月本金
+            var yuelixi = this.data.totalAmount * this.data.monthInterest / 100 //每月利息
+            var zonglixi = this.data.month * yuelixi//总利息
+            var zonghuankuanjine = parseInt(zonglixi) + parseInt(this.data.totalAmount)//总还款金额
+            var trueInterest = (24 * this.data.month * this.data.monthInterest / (parseInt(this.data.month) + 1)).toFixed(2)
+            this.setData(
+                {
+                    mingyinianlilv: mingyinianlilv + "%",//名义年利率
+                    trueInterest: trueInterest + "%",
+                    yuebenjin: yuebenjin,//每月本金
+                    yuelixi: yuelixi,//每月利息
+                    monthAmount: parseInt(yuebenjin) + parseInt(yuelixi),//每月还款金额
+                    zonglixi: zonglixi,//总利息
+                    zonghuankuanjine: zonghuankuanjine//总还款金额
+                }
+            )
+        }
     },
 
 
